f4d2283891c94d7d5274f144a81f51cab33c08e6
yanght5 <yanght5@lenovo.com>
2024-11-30 09:53:36 +0800

    整理成静态

--- MultiLanguagDemo/MultiLanguagDemo/StdAfx.h
+++ MultiLanguagDemo/MultiLanguagDemo/StdAfx.h
@@ -7,14 +7,19 @@
 
 #include "../../include/UIlib.h"
 using namespace DuiLib;
-#pragma comment(lib, "../../lib/Duilib_u.lib")
+//#pragma comment(lib, "../../lib/Duilib_u.lib")
+#ifdef _DEBUG
+#pragma comment(lib, "../../x64/Debug/DuiLib.lib")
+#else
+#pragma comment(lib, "../../x64/Release/DuiLib.lib")
+#endif // _DEBUG
 
 #include "../../Util/StringTools.h"
 #include "../../Util/ToolsFunc.h"
 
 //JSON
-#include "../json/include/json/json.h"
-#pragma comment(lib,"../json/lib/jsoncpp.lib")
+#include "json/json.h"
+//#pragma comment(lib,"../json/lib/jsoncpp.lib")
 
 extern wstring g_strAppPath;
 //∂‡”Ô—‘¥¶¿Ì
